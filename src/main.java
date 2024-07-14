93d97391174b16a2531e0f4277429bbd802371b1
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-07-14 09:40:21 +0530

    add employee

--- src/main.java
+++ src/main.java
@@ -92,8 +92,8 @@ public class main {
     public static void main(String[] args) {
         PayrollSystem payrollSystem = new PayrollSystem();
 
-        FullTimeEmployee emp1 = new FullTimeEmployee("John Doe", 101, 5000.0);
-        PartTimeEmployee emp2 = new PartTimeEmployee("Jane Smith", 102, 30, 15.0);
+        FullTimeEmployee emp1 = new FullTimeEmployee("Aditya Raj", 101, 500000.0);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Ayushman Singh", 102, 30, 15.0);
 
         payrollSystem.addEmployee(emp1);
         payrollSystem.addEmployee(emp2);
